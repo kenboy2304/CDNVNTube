c139f55a2c8aa0c11a35cfba527977095435f173
Việt Phạm <Vit Phm>
2017-10-17 11:16:55 +0700

    Add project files.

---
+++ CDNVNCMS.Tube/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CDNVNCMS.Tube.Startup))]
+namespace CDNVNCMS.Tube
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
